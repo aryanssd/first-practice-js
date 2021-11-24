02b7fbeb2b164730cc7c1f8519fa263c67940c5d
aryanpror8 <aryanjalai8466@gmail.com>
2021-11-24 18:52:25 +0330

    my-first-practice-of-java-script

---
+++ app.js
@@ -0,0 +1,111 @@
+// let x = 21;
+
+// if (x > 20){
+//     console.log('hello world');
+// }
+
+//========================== Write a program to check whether a given number is odd or even ==============================
+
+// let input = 58;
+// let remainder = input % 2;
+
+// if (remainder === 0){
+//     console.log(input + ' it is even');
+// }
+
+// else{
+//     console.log(input + ' it is odd');
+// }
+
+//=================FINISH THE FIRST PRACTICE=======================
+
+//======================Write a program to check whether a given number is positive or negative ==============================
+
+// let input = 7;
+
+// if (input < 0){
+//     console.log(input + ' is negative');
+// }
+
+// else {
+//     console.log(input + ' is positive');
+// }
+
+// =============================FINISH THE SECOND PRACTICE===============================
+
+// write a program to find the largest number ===================================
+
+// let input1 = 30;
+// let input2 = 25;
+// let input3 = 40;
+
+// if (input1 > input2) {
+//     if (input1 > input3) {
+//         console.log(input1 + ' is the largest');
+//     } else {
+//         console.log(input3 + ' is the largest');
+//     }
+// } else {
+//     if (input2 > input3) {
+//         console.log(input2 + ' is the largest');
+//     } else {
+//         console.log(input3 + ' is the largest');
+//     }
+// }
+
+/// ==================ANOTHER WAY===========================
+
+// let input1 = 30;
+// let input2 = 25;
+// let input3 = 40;
+
+// if ((input1 > input2) && (input1 > input3)){
+//     console.log(input1 + ' is the largest');
+// }
+// else if ((input2 > input1) && (input2 > input3)){
+//     console.log(input2 + ' is the largest');
+// }
+// else if ((input3 > input2) && (input3 > input1)){
+//     console.log(input3 + ' is the largest');
+// }
+// =============================== FINISH THE THIRD PRACTICE=======================================
+
+// write a program to accept a number and print the corresponding day.(1==>shanbe,2==>yek-shanbe,..)
+
+// let input = 1;
+
+// switch (input) {
+//     case 1:
+//         console.log('shanbe');
+//         break;
+//     case 2:
+//         console.log('yek-shanbe');
+//         break;
+//     case 3:
+//         console.log('do-shanbe');
+//         break;
+//     case 4:
+//         console.log('se-shanbe');
+//         break;
+//     case 5:
+//         console.log('char-shanbe');
+//         break;
+//     case 6:
+//         console.log('panj-shanbe');
+//         break;
+//     case 7:
+//         console.log('jomee');
+//         break;
+// }
+
+//=======================FINISH THE FORTH PRACTICE===========================
+
+//======================write a program to find the sum of first 10 natural numbers.================================
+
+// let sum = 0;
+
+// for(i = 1; i < 10; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
